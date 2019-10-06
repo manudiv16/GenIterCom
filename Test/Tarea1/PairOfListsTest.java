bf049f3d3b23fbcb7b7798e43ba729961af24788
manu <manudiv16@gmail.com>
2019-10-06 18:51:09 +0200

    realización de la tarea uno practica con listas y iteradores

---
+++ Test/Tarea1/PairOfListsTest.java
@@ -0,0 +1,38 @@
+package Tarea1;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PairOfListsTest {
+    @Test
+    void tes1() {
+        int pivot = 30;
+        List<Integer> lint = List.of(1, 23, 5, 42, 10, 3, 54, 22, 6);
+        PairOfLists pair = Partitioned.partition(lint, pivot);
+        assertTrue(checkMore(pair.getFirst(),pivot));
+        assertTrue(checkMenor(pair.getSecond(),pivot));
+    }
+
+    boolean checkMore(List<Integer> list, int pivot) {
+        for (Integer num : list) {
+            if (pivot < num) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    boolean checkMenor(List<Integer> list, int pivot) {
+        for (Integer num : list) {
+            if (num <= pivot) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
